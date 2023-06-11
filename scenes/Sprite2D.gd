dfff4b947a5e0cc08053171a33512b24c0356a75
Mo-BiuS <lodland95@gmail.com>
2023-06-11 17:13:14 +0200

    let's do a small breakout game!

--- scenes/Sprite2D.gd
+++
@@ -1,9 +0,0 @@
-extends Sprite2D
-
-var displacement:Vector2 = Vector2(0,0)
-
-func _process(delta):
-	if displacement.x < 0 && position.x > 0 : position.x+=displacement.x
-	if displacement.y < 0 && position.y > 0 : position.y+=displacement.y
-	if displacement.x > 0 && position.x < 320 : position.x+=displacement.x
-	if displacement.y > 0 && position.y < 480 : position.y+=displacement.y
